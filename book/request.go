f4caa462c82ea9555d6941615867e266dd66921b
Ajulll22 <ajulrizki@gmail.com>
2022-05-26 19:28:31 +0700

    request books

---
+++ book/request.go
@@ -0,0 +1,10 @@
+package book
+
+import "encoding/json"
+
+type BookRequest struct {
+	Title       string      `json:"title" binding:"required"`
+	Price       json.Number `json:"price" binding:"required,number"`
+	Description string      `json:"description" binding:"required"`
+	Rating      json.Number `json:"rating" binding:"required,number"`
+}
